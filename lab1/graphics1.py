304d97a46bc2f55822e6b2bd04c8a9039173049a
Ilya <selivanov.i@abcloudz.com>
2021-09-26 22:31:26 +0300

    Update diagrams

--- lab1/graphics1.py
+++ lab1/graphics1.py
@@ -7,11 +7,11 @@ import pandas as pd
 # Read file ham_length_frequency
 ham_word_df = pd.read_csv(r'out\ham\ham_groupby_length_frequency.csv')
 
-ham_word_df['Percent'] = (ham_word_df['Frequency'] / ham_word_df['Frequency'].sum()) * 100
+#ham_word_df['Percent'] = (ham_word_df['Frequency'] / ham_word_df['Frequency'].sum()) * 100
 
 spam_word_df = pd.read_csv(r'out\spam\spam_groupby_length_frequency.csv')
 
-spam_word_df['Percent'] = (spam_word_df['Frequency'] / spam_word_df['Frequency'].sum()) * 100
+#spam_word_df['Percent'] = (spam_word_df['Frequency'] / spam_word_df['Frequency'].sum()) * 100
 
 
 mean_ham_df = pd.read_csv(r'out\ham\ham_length_frequency.csv')
@@ -23,8 +23,8 @@ mean_word_df = pd.concat([mean_ham_df, mean_spam_df])
 mean_word = round(mean_word_df['Length'].mean())
 
 plt.title('Words')
-plt.bar(ham_word_df['Length'], ham_word_df['Percent'])
-plt.bar(spam_word_df['Length'], spam_word_df['Percent'])
+plt.bar(ham_word_df['Length'], ham_word_df['Frequency'])
+plt.bar(spam_word_df['Length'], spam_word_df['Frequency'])
 plt.axvline(x=mean_word, linestyle='--', color="Green")
 plt.xlabel('length')
 plt.ylabel('frequency, %')
--- lab1/graphics2.py
+++ lab1/graphics2.py
@@ -6,13 +6,12 @@ import pandas as pd
 
 ham_sentence_df = pd.read_csv(r'out\ham\ham_sentence_frequency.csv')
 
-ham_sentence_df['Percent'] = (ham_sentence_df['0'] / ham_sentence_df['0'].sum()) * 100
+#ham_sentence_df['Percent'] = (ham_sentence_df['0'] / ham_sentence_df['0'].sum()) * 100
 
-print(ham_sentence_df['Percent'])
 
 spam_sentence_df = pd.read_csv(r'out\spam\spam_sentence_frequency.csv')
 
-spam_sentence_df['Percent'] = (round(spam_sentence_df['0'] / spam_sentence_df['0'].sum())) * 100
+#spam_sentence_df['Percent'] = (round(spam_sentence_df['0'] / spam_sentence_df['0'].sum())) * 100
 
 mean_ham_df = pd.read_csv(r'out\ham\ham.csv')
 mean_spam_df = pd.read_csv(r'out\spam\spam.csv')
@@ -24,8 +23,8 @@ mean_sentence = round(mean_sentence_df['Length'].mean())
 
 plt.title('Sentence')
 
-plt.bar(ham_sentence_df['Length'], ham_sentence_df['Percent'])
-plt.bar(spam_sentence_df['Length'], spam_sentence_df['Percent'])
+plt.bar(ham_sentence_df['Length'], ham_sentence_df['0'])
+plt.bar(spam_sentence_df['Length'], spam_sentence_df['0'])
 plt.axvline(x=mean_sentence, linestyle='--', color="Green")
 plt.xlabel('length')
 plt.ylabel('frequency, %')
